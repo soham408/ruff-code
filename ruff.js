37e2e54567fb9f2078192a6c95324215513a320d
Soham Bambade <bambadesoham408@gmail.com>
2024-07-19 08:31:08 +0000

    ruff code

--- ruff.js
+++ ruff.js
@@ -1,8 +1,33 @@
-for (let i = 1; i <= 10; i++) {
-    //console.log(`Outer loop value: ${i}`);
-   for (let j = 1; j <= 10; j++) {
-    //console.log(`Inner loop value ${j} and inner loop ${i}`);
-    console.log(i + '*' + j + ' = ' + i*j );
-   }
-    
+let Number = -1
+
+let Num = `${Number > 0 || Number < 0 ? "positive" : "Negetive"}`
+
+// console.log(Num);
+
+let number = 0;
+let result = number > 0 ? "Positive" : (number < 0 ? "Negative" : "Zero");
+// console.log(result);
+
+
+// ================ if else ===================
+
+let Numb = -1
+
+if (Numb > 0) {
+//    console.log("positive");
+}else if(Numb < 0) {
+    // console.log("negitive");
+}else {
+    // console.log("zero");
 }
+
+
+
+let age = 10
+
+if (age == 18 || age > 18) {
+    // console.log("eligible for vote");
+}else if (age < 18) {
+    // console.log("not eligible for vote");
+}
+
